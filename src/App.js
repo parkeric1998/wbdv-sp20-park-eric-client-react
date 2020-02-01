a4f14834c31cb7d1b686e26819faf6d0a0eaf346
Eric Park <park.er@husky.neu.edu>
2020-02-01 16:23:28 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CourseManagerComponent from "./components/CourseManagerComponent";
+
+const App = () =>
+    <CourseManagerComponent/>
+
+
+export default App;
